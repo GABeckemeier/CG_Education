ba37e8799f8493ba3e8cbd6f550be28cdc01d289
GBeckemeier <gabbybeckemeier@gmail.com>
2018-05-16 07:15:54 -0500

    First revisit of JS Challenge II - Incomplete

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
-console.log('Yayyy!');
-var Teacher = ('Teacher: Jack Sparrow');
-var Department = ('Skill: Drinking');
-var Rating = ('Rating:');
-var Rating1 = (4.3);
-var Rating2 = (2.5);
-var Rating3 = (2.0);
+//Captains Page Calculations Example
+var Teacher = "Teacher: Jack Sparrow";
+var Department = "Skill: Drinking";
+var Rating = ('Rating: ');
+var jsRatings = [4.3, 2.5, 2.0];
+function getRatingAvg (jsRatings) {
+  var avg = jsRatings.reduce((total, amount) => total += amount) / jsRatings.length;
+  return jsRatings;
+  }
+console.log(getRatingAvg(jsRatings));
+
+
+function addTeacherRating (jsRatings, newRating) {
+  newRating =
+  jsRatings.push(newRating);
+  return jsRatings;
+}
 console.log(Teacher);
 console.log(Department);
-console.log(Rating, Rating1, Rating2, Rating3);
-console.log(Rating1 + Rating2 + Rating3 / 3);
+console.log(Rating);
+//Skills Page Variables Example
+var Course = ('Swashbuckling');
+var ChingShih = ('Ching Shih');
+var Term = ('Summer 2018');
+//Scallywags Page Calculations Example
+var ABonny = ('Anne Bonny');
+var ABmail = ('ABonny@gmail.com');
+var ABcourse = ('Brawling', 'Drinking');
+var ABGpa = (4.5);
+var ETGpa = (4.0);
+var FDGpa = (3.6);
+var GpaAddition = (ABGpa + ETGpa + FDGpa);
+var GpaDivide = (GpaAddition / 3);
+console.log(GpaAddition);
+console.log(GpaDivide);
